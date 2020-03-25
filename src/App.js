1608610e6a9420894e92e2ad6b17ce02196ff5b7
Ryan Savoca <ryan.p.savoca@gmail.com>
2020-03-25 12:53:32 -0700

    task todo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import * as serviceWorker from './serviceWorker';
 
 import Form from "./js/components/Form";
+import Todo from "./js/components/ToDo";
 import './App.css';
 
 function App() {
   return (
-    <Form />
+    <Todo />
   );
 }
 
 export default App;
+
+serviceWorker.unregister();
